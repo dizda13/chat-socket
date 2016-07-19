2b3c7b17351904f25c2b0959987bc74857514e96
dizda13 <dino_dizda@live.com>
2016-07-19 13:49:53 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by Dino on 19.7.2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        String nesto=new String();
+        Scanner sc=new Scanner(System.in);
+        nesto=sc.next();
+        System.out.println(nesto);
+
+    }
+}
